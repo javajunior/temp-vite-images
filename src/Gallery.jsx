1a378fefc7e2adada04fd1e4ed89bfed24c29a3d
Gianluca Giavatto <gianluca.giavatto@gmail.com>
2023-12-15 17:44:44 +0100

    full project

---
+++ src/Gallery.jsx
@@ -0,0 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
+
+const Gallery = () => {
+  const { search } = useGlobalContext();
+
+  const response = useQuery({
+    queryKey: ["images", search],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${search}`);
+      return result.data;
+    },
+  });
+
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            key={item.id}
+            src={url}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+export default Gallery;
---
+++ src/context.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const storedDarkMode = localStorage.getItem("darkTheme");
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode;
+  }
+
+  return storedDarkMode === "true";
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [search, setSearch] = useState("cat");
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, search, setSearch }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
